05a6560f42133b91829e8811d615c62b8fd4a63e
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-15 15:23:31 +0530

    Check Charater is Vowel and Consonent

---
+++ VowelAndConsonent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class VowelAndConsonent
+    {
+        public static void CheckVowelAndConstant()
+        {
+            char ch;
+            Console.WriteLine("Enter any Character");
+            ch = Convert.ToChar(Console.ReadLine());
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                Console.WriteLine(ch + "is a vowel");
+            }
+            else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+            {
+                Console.WriteLine(ch + " is a Consonant");
+
+
+            }
+            else
+                {
+                    Console.WriteLine("Please Enter Valid Charcter");
+                }
+        }    
+    }
+}
