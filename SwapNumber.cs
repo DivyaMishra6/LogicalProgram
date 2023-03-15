4d5b5f0d9b803804d8abef15c0f24902def6fb73
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-15 15:15:25 +0530

    Swap the two Number

---
+++ SwapNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class SwapNumber
+    {
+        public static void DisplaySwapNumber()
+        {
+            int a , b;
+
+            Console.WriteLine("Enter First Number");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter First Number");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Before swap a=" + a + " b =" + b);
+
+            a = a * b;
+            b = a / b;
+            a = a / b;
+
+            Console.WriteLine("After swap a = " + a + " b = " + b);
+        }
+    }
+}
