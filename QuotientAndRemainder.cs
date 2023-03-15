5fd78eb50fa095e2bb5e71ec0c90a12092e4d71a
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-15 15:20:55 +0530

    Check Quotient and Remainder

---
+++ QuotientAndRemainder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class QuotientAndRemainder
+    {
+        public static void FindQuotientAndRemainder()
+        {
+            int divident;
+            int divisor;
+
+            Console.WriteLine("Enter the divident");
+            divident = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the divisor");
+            divisor = Convert.ToInt32(Console.ReadLine());
+
+            int remainder = divident % divisor;
+            int Quotient = divident / divisor;
+
+            Console.WriteLine("The divident" + divident + "and divisor " + divisor) ;
+
+            Console.WriteLine("The quotent is " + Quotient + "and The reminder is " + remainder);
+
+
+        }
+    }
+}
